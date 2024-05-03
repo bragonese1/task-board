3fa4430c2fc7bc94868cd6ebf0ae773704da551a
bradragz <ragonesebradley@gmail.com>
2024-05-02 21:12:34 -0700

    creating js functions to create task card and make it draggable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Retrieve tasks and nextId from localStorage
+import { task } from 'ember-concurrency';
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
@@ -22,16 +23,60 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+const taskCard = $('<div>').addClass('card task-card draggable my-3')
+.attr('data-task-id', task.id);
+const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
+const cardBody = $('<div>').addClass('card-body');
+const cardDescription = $('<div>').addClass('card-text').text(task.description);
+const cardDueDate = $('<div>').addClass('card-text').text(task.dueDate);
 
+cardBody.append(cardDescription, cardDueDate);
+taskCard.append(cardHeader, cardBody);
+
+return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    const todo = $(".todo-list");
+    const inProgress = $(".in-progress-list");
+    const done = $(".done-list");
+    
+for(let task of taskList){
+    if(task.status == 'to-do'){
+        todo.append(createTaskCard(task));
+    }else if(task.status == 'in-progress'){
+        inProgress.append(createTaskCard(task));
+    }else if(task.status == 'done'){
+        done.append(createTaskCard(task));
+    }
 }
+
+//draggable using jquery
+
+$('.draggable').draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    // This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+    helper: function (e) {
+      // Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+      const original = $(e.target).hasClass('ui-draggable')
+        ? $(e.target)
+        : $(e.target).closest('.ui-draggable');
+      // Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+      return original.clone().css({
+        width: original.outerWidth(),
+      });
+    },
+  });
+}
+
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    // Prevent the default form submission behavior
+    event.preventdefualt();
+   
 
 }
 
